749a328b66d99941da9bea8e14243b69c50b8bf2
Nooshin bakhtiari <nooshinbakhtiariii@gmail.com>
2023-01-14 23:18:47 +0330

    add fiture to project

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+const btnMenu = document.querySelector('.header__menu');
+const navLinks = document.querySelector('.main-list');
+btnMenu.addEventListener('click' , function(){
+ 
+  if(btnMenu.classList.contains('close')){
+    btnMenu.classList.remove('close');
+  } else{
+    btnMenu.classList.add('close');
+    console.log(navLinks);
+    navLinks.style.display = 'block';
+   
+
+  }
+  
+});
+
+
+
