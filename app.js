bcc337594d564e6022215c22123a985795412fa3
Nooshin bakhtiari <nooshinbakhtiariii@gmail.com>
2023-01-16 12:47:11 +0330

    add navbar toggle

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-'use strict';
+const hambuerger =document.querySelector('.hamburger');
+const navMenu = document.querySelector('.mobile-nav');
 
+hambuerger.addEventListener('click' , function(){
+  hambuerger.classList.toggle('is-active');
+  navMenu.classList.toggle('is-active');
 
-const btnMenu = document.querySelector('.header__menu');
-const navLinks = document.querySelector('.main-list');
-btnMenu.addEventListener('click' , function(){
- 
-  if(btnMenu.classList.contains('close')){
-    btnMenu.classList.remove('close');
-  } else{
-    btnMenu.classList.add('close');
-    console.log(navLinks);
-    navLinks.style.display = 'block';
-   
-
-  }
-  
-});
-
-
-
+})
