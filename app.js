0e5762661fc7685e064fb6240dd49b0cebd4a98e
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-02 21:38:38 +0330

    delete all the extra files

--- app.js
+++
@@ -1,8 +0,0 @@
-const hambuerger =document.querySelector('.hamburger');
-const navMenu = document.querySelector('.mobile-nav');
-
-hambuerger.addEventListener('click' , function(){
-  hambuerger.classList.toggle('is-active');
-  navMenu.classList.toggle('is-active');
-
-})
